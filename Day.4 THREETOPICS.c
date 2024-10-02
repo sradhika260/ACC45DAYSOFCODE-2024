5650c56545bce2c2f2abf065e7cba19543dde59c
sradhika260 <sikarwarradhika260@gmail.com>
2024-10-02 20:36:17 +0530

    Add files via upload

---
+++ Day.4 THREETOPICS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(){
+     // your code goes here
+      int A,B,C,X;
+      scanf("%d %d %d %d",&A,&B,&C,&X);
+      if (X==A || X==B ||X==C){
+          printf("yes\n");
+      }
+      else{
+          printf("no\n");
+     
+      } 
+};
+ 
+
+
+
